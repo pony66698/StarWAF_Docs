5041cebcb9b4a690986f0b1fb13074f470745fe7
Sh1n3zZ <DengJunHai@foxmail.com>
2023-07-12 13:21:37 +0800

    Update index.tsx

--- Website/src/pages/index.tsx
+++ Website/src/pages/index.tsx
@@ -37,6 +37,17 @@ function HomepageHeader() {
           针对 <strong>中国大陆环境进行链路优化</strong> 采用大量边缘 CDN 和 CN2 GIA 高速链路保证节点与中国的高性能链接 .
         </p>
         <span data-aos="zoom-y-out" data-aos-delay="300">
+        
+        <Link
+          className={clsx(
+            "button button--secondary button--lg",
+            styles.heroButton
+          )}
+          to="/docs/"
+        >
+          了解更多
+        </Link>
+        
         <a
           className={clsx(
             "button button--primary button--lg",
@@ -48,16 +59,6 @@ function HomepageHeader() {
         >
         立即接入 <span className="buttonIcon"><svg className="arrowhome" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 448 512" font-size="0.8em" aria-hidden="true" focusable="false" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z"></path></svg></span>
         </a>
-
-        <Link
-          className={clsx(
-            "button button--secondary button--lg",
-            styles.heroButton
-          )}
-          to="/docs/"
-        >
-          了解更多
-        </Link>
 
         <a
           className={clsx(
